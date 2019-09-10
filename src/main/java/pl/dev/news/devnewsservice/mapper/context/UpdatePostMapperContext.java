01f6a8b7e3dfbafef395bc34a9521802a7886b72
Artsem Kniazeu <artsem.kniazeu@gmail.com>
2019-09-10 12:22:39 +0200

    remove uploads from model

--- src/main/java/pl/dev/news/devnewsservice/mapper/context/UpdatePostMapperContext.java
+++ src/main/java/pl/dev/news/devnewsservice/mapper/context/UpdatePostMapperContext.java
@@ -5,7 +5,6 @@ import org.mapstruct.MappingTarget;
 import pl.dev.news.devnewsservice.entity.PostEntity;
 import pl.dev.news.devnewsservice.mapper.CategoryMapper;
 import pl.dev.news.devnewsservice.mapper.TagMapper;
-import pl.dev.news.devnewsservice.mapper.UploadMapper;
 import pl.dev.news.model.rest.RestPostModel;
 
 import java.util.HashSet;
@@ -32,12 +31,6 @@ public class UpdatePostMapperContext implements PostMapperContext {
             model.getCategories().stream()
                     .map(CategoryMapper.INSTANCE::toEntity)
                     .forEach(entity::addCategory);
-        }
-        if (model.getUploads() != null) {
-            entity.setUploads(new HashSet<>());
-            model.getUploads().stream()
-                    .map(UploadMapper.INSTANCE::toEntity)
-                    .forEach(entity::addUpload);
         }
     }
 
